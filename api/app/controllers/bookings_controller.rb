7dcbe0542b09bb1b9cc72d22a2db0064cbb9f83b
Baptiste BOUCHER <bboucher@e1r1p9.42.fr>
2019-07-09 08:23:00 +0200

    CRUD in bookings controller

--- api/app/controllers/bookings_controller.rb
+++ api/app/controllers/bookings_controller.rb
@@ -1,2 +1,42 @@
 class BookingsController < ApplicationController
+    before_action :set_booking, only: [:show, :update, :destroy]
+    
+    def index         # GET /bookings
+        @bookings = Booking.all
+        render json: @bookings
+    end
+
+    def show          # GET /bookings/:id
+        render json: @booking        
+    end
+
+    def create        # POST /bookings
+        @booking = Booking.new(booking_params)
+        if @booking.save
+            render json: @booking   
+        else 
+            render json: {msg: "booking aborted"}
+        end     
+    end
+
+    def update        # PATCH /bookings/:id
+        @booking.update(booking_params)
+        render json: @booking        
+    end
+
+    def destroy       # DELETE /bookings/:id
+        id = @booking.id
+        @booking.destroy
+        render json: {msg: "booking ##{id} deleted"}
+    end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit(:listing_id, :start_date, :end_date)
+    end
+
+    def set_booking
+        @booking = Booking.find(params[:id])
+    end
 end
