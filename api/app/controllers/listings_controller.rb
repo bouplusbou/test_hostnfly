8b48dc2c800a510d7f2200bf07a96598b4def002
Baptiste BOUCHER <bboucher@e1r1p9.42.fr>
2019-07-09 08:16:18 +0200

    CRUD in listings controller

--- api/app/controllers/listings_controller.rb
+++ api/app/controllers/listings_controller.rb
@@ -1,2 +1,40 @@
 class ListingsController < ApplicationController
+    before_action :set_listing, only: [:show, :update, :destroy]
+    
+    def index         # GET /listings
+        @listings = Listing.all
+        render json: @listings
+    end
+
+    def show          # GET /listings/:id
+        render json: @listing        
+    end
+
+
+    def create        # POST /listings
+        @listing = Listing.new(listing_params)
+        @listing.save
+        render json: @listing        
+    end
+
+    def update        # PATCH /listings/:id
+        @listing.update(listing_params)
+        render json: @listing        
+    end
+
+    def destroy       # DELETE /listings/:id
+        id = @listing.id
+        @listing.destroy
+        render json: {msg: "listing ##{id} deleted"}
+    end
+
+    private
+
+    def listing_params
+        params.require(:listing).permit(:num_rooms)
+    end
+
+    def set_listing
+        @listing = Listing.find(params[:id])
+    end
 end
