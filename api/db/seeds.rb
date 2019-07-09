0069c32386b637b20c35d28a1e951ef095f03b8b
Baptiste BOUCHER <bboucher@e1r1p9.42.fr>
2019-07-09 08:13:00 +0200

    db migrate and seed from input data

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+input = {
+  "listings": [
+    { "id": 1, "num_rooms": 2 },
+    { "id": 2, "num_rooms": 1 },
+    { "id": 3, "num_rooms": 3 }
+  ],
+  "bookings": [
+    { "id": 1, "listing_id": 1, "start_date": "2016-10-10", "end_date": "2016-10-15" },
+    { "id": 2, "listing_id": 1, "start_date": "2016-10-16", "end_date": "2016-10-20" },
+    { "id": 3, "listing_id": 2, "start_date": "2016-10-15", "end_date": "2016-10-20" }
+  ],
+  "reservations": [
+    { "id": 1, "listing_id": 1, "start_date": "2016-10-11", "end_date": "2016-10-13" },
+    { "id": 2, "listing_id": 1, "start_date": "2016-10-13", "end_date": "2016-10-15" },
+    { "id": 3, "listing_id": 1, "start_date": "2016-10-16", "end_date": "2016-10-20" },
+    { "id": 4, "listing_id": 2, "start_date": "2016-10-15", "end_date": "2016-10-18" }
+  ]
+}
+
+puts 'Creating listings...'
+input[:listings].each do |listing|
+    new_listing = Listing.new(**listing)
+    new_listing.save!
+end
+puts 'Finished!'
+
+puts 'Creating bookings...'
+input[:bookings].each do |booking|
+    new_booking = Booking.new(**booking)
+    new_booking.save!
+end
+puts 'Finished!'
+
+puts 'Creating reservations...'
+input[:reservations].each do |reservation|
+    new_reservation = Reservation.new(**reservation)
+    new_reservation.save!
+end
+puts 'Finished!'
