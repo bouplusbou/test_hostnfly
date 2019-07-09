c25e49b3a597bae2940157ce7cf86dab83aea16d
Baptiste BOUCHER <bboucher@e1r1p9.42.fr>
2019-07-09 09:06:31 +0200

    check on bookings overlap in Booking model

--- api/app/models/booking.rb
+++ api/app/models/booking.rb
@@ -1,3 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :listing
+
+  before_save :check_dates
+
+  def check_dates
+    valid_dates = true
+    existing_bookings = Booking.where(listing_id: listing_id)
+    existing_bookings.each do |booking|
+      if booking.start_date < end_date && booking.end_date > start_date
+        valid_dates = false
+      end
+    end
+    valid_dates ? '' : throw(:abort)
+  end
 end
